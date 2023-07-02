f93e8d079633a004d40c94b505fc4e92a95c7f88
aman-reso <amansecxcosx@gmail.com>
2023-07-02 22:41:38 +0530

    zust electronics

--- app/src/main/java/in/opening/area/zustapp/storage/db/UserDao.kt
+++ app/src/main/java/in/opening/area/zustapp/storage/db/UserDao.kt
@@ -2,10 +2,10 @@ package `in`.opening.area.zustapp.storage.db
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy
 import `in`.opening.area.zustapp.data.UserModel
 @Dao
 interface UserDao {
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun setUser(userModel: UserModel)
 }
